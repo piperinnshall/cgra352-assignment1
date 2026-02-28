3d9de4395ab20f94b3046cb52fe52a5b30ad555b
Piper Inns Hall <piper.innshall@gmail.com>
2026-03-01 00:44:03 +1300

    chore: Get hsv image

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
+use opencv::{
+    core::{AlgorithmHint, Mat, MatTraitConst, Vector},
+    imgcodecs, imgproc,
+};
 use std::error::Error;
 
-use opencv::{
-    core::{self, Mat, Vector},
-    imgcodecs,
-};
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let source_img = imgcodecs::imread("car.png", imgcodecs::IMREAD_UNCHANGED)?;
+    let params = Vector::new();
+    let flower_img = imgcodecs::imread("assets/Flower.jpg", imgcodecs::IMREAD_UNCHANGED)?;
 
-    // Flipping image horizontally
-    // let mut destination_arr = Mat::default();
-    // core::flip(&source_img, &mut destination_arr, 1)?;
+    let hsv_img = convert_to_hsv(&flower_img)?;
+    let width = hsv_img.cols() * 3;
 
-    // Creating an output image
-    let arguments: Vector<i32> = Vector::new();
-    imgcodecs::imwrite("final-output.png", &destination_arr, &arguments)?;
+    imgcodecs::imwrite("hsv.jpg", &hsv_img, &params)?;
+
     Ok(())
+}
+
+// fn split_bgr(mat: &Mat
+
+fn convert_to_hsv(material: &Mat) -> Result<Mat, Box<dyn Error>> {
+    let mut dest_material = Mat::default();
+    imgproc::cvt_color(
+        material,
+        &mut dest_material,
+        imgproc::COLOR_BGR2HSV,
+        0,
+        AlgorithmHint::ALGO_HINT_DEFAULT,
+    )?;
+    Ok(dest_material)
 }
