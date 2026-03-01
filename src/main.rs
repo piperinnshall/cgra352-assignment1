9574b56fad5f74fc208e63b555e7b80292d6a34a
Piper Inns Hall <piper.innshall@gmail.com>
2026-03-01 16:37:34 +1300

    chore: Split and multiply channels

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,49 @@
+use anyhow::{anyhow, Result};
 use opencv::{
     core::{
         AlgorithmHint, Mat, MatExprTraitConst, MatTraitConst, Rect, Vector, VectorToVec,
     },
     imgcodecs, imgproc,
 };
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let params = Vector::new();
     core(params)?;
     Ok(())
 }
 
-fn core(params: Vector<i32>) -> Result<(), Box<dyn Error>> {
+fn core(params: Vector<i32>) -> Result<()> {
     let flower_bgr = imgcodecs::imread("assets/Flower.jpg", imgcodecs::IMREAD_UNCHANGED)?;
     let flower_hsv = convert_to_hsv(&flower_bgr)?;
 
-    let mut images = Vec::new();
-    images.append(&mut split_channels(&flower_bgr)?);
-    images.append(&mut split_channels(&flower_hsv)?);
+    let flower_bgr_split = split_channels(&flower_bgr)?;
+    let flower_hsv_split = split_channels(&flower_hsv)?;
 
-    let large_image_height = 2;
-    let large_image_width = 3;
-    let large_image = create_large_image(&images, &large_image_height, &large_image_width)?;
+    // Core 1
+    let mut core_1 = Vec::new();
+    core_1.extend(flower_bgr_split.clone());
+    core_1.extend(flower_hsv_split.clone());
 
-    imgcodecs::imwrite("assets/Core1.jpg", &large_image, &params)?;
+    let large = create_large_image(&core_1, 2, 3)?;
+    imgcodecs::imwrite("assets/Core1.jpg", &large, &params)?;
+
+    // Core 2
+    // let mut core_2 = Vec::new();
+    
+    // merge_channels(Vector::from_slice());
+    // core_2.extend(multiply_channel(&flower_hsv_split[0].clone())?); // Hue
+    // core_2.extend(multiply_channel(&flower_hsv_split[1].clone())?); // Saturation
+    // core_2.extend(multiply_channel(&flower_hsv_split[2].clone())?); // Value
+
+    imgcodecs::imwrite("assets/Core2.jpg", &core_2[0], &params)?;
 
     Ok(())
 }
 
-/** 
-Converts a BGR image Matrix to an HSV image Matrix
+/**
+Converts a BGR image Matrix to an HSV image Matrix.
 */
-fn convert_to_hsv(m: &Mat) -> Result<Mat, Box<dyn Error>> {
+fn convert_to_hsv(m: &Mat) -> Result<Mat> {
     let mut dst = Mat::default();
     let dst_cn = 0;
     let code = imgproc::COLOR_BGR2HSV;
@@ -41,28 +52,55 @@ fn convert_to_hsv(m: &Mat) -> Result<Mat, Box<dyn Error>> {
     Ok(dst)
 }
 
-/** 
-Divides a multi-channel Matrix into several single-channel Matrices
+/**
+Divides a multi-channel Matrix into several single-channel ones.
 */
-fn split_channels(m: &Mat) -> Result<Vec<Mat>, Box<dyn Error>> {
+fn split_channels(m: &Mat) -> Result<Vec<Mat>> {
     let mut mv: Vector<Mat> = vec![Mat::default(); m.channels() as usize].into();
     opencv::core::split(m, &mut mv)?;
     Ok(mv.to_vec())
 }
 
-/** 
-Creates a large image from of a grid of small same-sized images and a large base image
+/**
+ * 
+Creates one multi-channel Matrix out of several single-channel ones.
+*/
+fn merge_channels(mv: &Vector<Mat>) -> Result<Mat> {
+    let mut m = Mat::default();
+    opencv::core::merge(mv, &mut m);
+    Ok(m)
+}
+
+/**
+Multiplies a single-channel Matrix by 0.0, 0.2, 0.4, 0.6, and 0.8.
+*/
+fn multiply_channel(m: &Mat) -> Result<Vec<Mat>> {
+    [0.0, 0.2, 0.4, 0.6, 0.8]
+        .iter()
+        .map(|&mult| {
+            (m * mult)
+                .into_result()?
+                .to_mat()
+                .map_err(|e| anyhow!(e.to_string()))
+        })
+        .collect()
+}
+
+/**
+Creates a large image from of a grid of small same-sized images and a large base image.
 */
 fn create_large_image(
     images: &Vec<Mat>,
-    large_image_height: &i32,
-    large_image_width: &i32,
-) -> Result<Mat, Box<dyn Error>> {
-    let small_image = images.get(0).ok_or("Error: Images vector is empty")?;
+    large_image_height: i32,
+    large_image_width: i32,
+) -> Result<Mat> {
+    let small_image = images
+        .get(0)
+        .ok_or_else(|| anyhow!("Error: Images vector is empty"))?;
     let height = small_image.rows() * large_image_height;
     let width = small_image.cols() * large_image_width;
 
-    // Creates a zero-initialized single channel (CV_8UC1) Matrix 
+    // Creates a zero-initialized single channel (CV_8UC1) Matrix
     let mut large_image = Mat::zeros(height, width, opencv::core::CV_8UC1)?.to_mat()?;
 
     for (idx, m) in images.iter().enumerate() {
